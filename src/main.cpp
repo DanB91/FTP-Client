ccba7a313fc770a6e07c1a582edfb60f1533a37e
Dan <dan.bokser@gmail.com>
2012-06-25 21:26:20 -0600

    added makefile and main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define HOSTNAME 1
+#define PORT 2
+#define MAX_ARGS 3
+#define MIN_ARGS 2
+
+void mainLoop(char *hostname, char *port)
+{
+
+}
+
+
+int main(int argc, char **argv)
+{
+	//check for correct args
+	if(argc < MIN_ARGS || argc > MAX_ARGS)
+		std::cout << "Usage cli hostname [port]" << std::endl;
+	else
+		mainLoop(argv[HOSTNAME], argv[PORT]);
+
+
+	return 0;
+}
